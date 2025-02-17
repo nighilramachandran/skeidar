7770b1f6d37459376bb4cb5cb4e3056c62b26e29
nighilramachandran <nighilramm@gmail.com>
2025-02-17 12:08:56 +0400

    next config for images from skeidar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -5,12 +5,20 @@ import {
   CampaingStyles,
   headerStyles,
 } from "./utils/LayoutConfig";
+import Image from "next/image";
 
 const Navbar: React.FC = () => {
   return (
     <AppBar sx={{ ...appBarStyles }}>
       <CampaignMessage />
-      <Toolbar disableGutters sx={{ ...headerStyles }}></Toolbar>
+      <Toolbar disableGutters sx={{ ...headerStyles }}>
+        <Image
+          src={"https://www.skeidar.no/globalassets/skeidar-logo_white.png"}
+          alt="skeidar-logo_white"
+          width={300}
+          height={30}
+        ></Image>
+      </Toolbar>
     </AppBar>
   );
 };
