9498df683586728e89898739f24706a675aa0b05
nighilramachandran <nighilramm@gmail.com>
2025-02-18 17:23:48 +0400

    footer completed

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -2,7 +2,6 @@ import { Box, Stack, Typography } from "@mui/material";
 import Image from "next/image";
 import React from "react";
 import CustomeContainer from "./components/CustomeContainer";
-import Logo from "./components/Logo";
 import {
   imagePhoneSrc,
   imagePostnordUrl,
@@ -30,52 +29,82 @@ const Footer: React.FC = () => {
       }}
     >
       <CustomeContainer>
-        <Stack spacing={5} justifyContent={"center"} alignItems={"center"}>
-          <Box
-            display={"flex"}
-            flexDirection="row"
-            flexWrap="wrap"
-            width="100%"
-            sx={{
-              justifyContent: "center",
-              gap: { xs: 10, sm: 10, lg: 15, xl: 20 },
-            }}
-          >
-            {[...Array(5)].map((_, index) => (
-              <Stack textAlign={"center"} spacing={2} key={index}>
-                <Image
-                  key={index}
-                  alt="landscaped_phone"
-                  src={imagePhoneSrc}
-                  height={100}
-                  width={100}
-                />
-                <Typography>{footerItems[index]}</Typography>
-              </Stack>
-            ))}
-          </Box>
-          <Logo />
-          <Box
-            display={"flex"}
-            flexDirection="row"
-            flexWrap="wrap"
-            width="100%"
-            sx={{ justifyContent: "center", gap: 10 }}
-          >
-            {footerImageItems?.map((imgurl, index) => {
-              return (
-                <Image
-                  key={index}
-                  alt="landscaped_phone"
-                  src={imgurl}
-                  height={100}
-                  width={130}
-                />
-              );
-            })}
-          </Box>
+        <Stack spacing={3} justifyContent={"center"} alignItems={"center"}>
+          <SectionOne />
+          <SectionTwo />
+          <SectionThree />
         </Stack>
       </CustomeContainer>
+    </Box>
+  );
+};
+
+const SectionOne: React.FC = () => {
+  return (
+    <Box
+      display={"flex"}
+      flexDirection="row"
+      flexWrap="wrap"
+      width="100%"
+      sx={{
+        justifyContent: "center",
+        gap: { xs: 10, sm: 10, lg: 15, xl: 20 },
+      }}
+    >
+      {[...Array(5)].map((_, index) => (
+        <Stack textAlign={"center"} spacing={2} key={index}>
+          <Image
+            key={index}
+            alt="landscaped_phone"
+            src={imagePhoneSrc}
+            height={100}
+            width={100}
+          />
+          <Typography>{footerItems[index]}</Typography>
+        </Stack>
+      ))}
+    </Box>
+  );
+};
+const SectionTwo: React.FC = () => {
+  return (
+    <Box
+      sx={{
+        position: "relative",
+        width: "100%",
+        maxWidth: "700px",
+        height: "135px",
+      }}
+    >
+      <Image
+        alt="footer_image"
+        src="/images/footer_image.png"
+        fill
+        style={{ objectFit: "contain" }}
+      />
+    </Box>
+  );
+};
+const SectionThree: React.FC = () => {
+  return (
+    <Box
+      display={"flex"}
+      flexDirection="row"
+      flexWrap="wrap"
+      width="100%"
+      sx={{ justifyContent: "center", gap: { xs: 2, sm: 10, lg: 15, xl: 20 } }}
+    >
+      {footerImageItems?.map((imgurl, index) => {
+        return (
+          <Image
+            key={index}
+            alt="landscaped_phone"
+            src={imgurl}
+            height={100}
+            width={130}
+          />
+        );
+      })}
     </Box>
   );
 };
