c0a901c4dc1e8fd3e46a81befd3d649d0e3d9d23
nighilramachandran <nighilramm@gmail.com>
2025-02-18 10:55:23 +0400

    remove unused domains from next config

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      "www.skeidar.no",
-      "www.freepik.com",
-      "img.freepik.com",
-      "www.google.com",
-      "png.pngtree.com",
-    ],
+    domains: ["www.skeidar.no"],
   },
 };
 
