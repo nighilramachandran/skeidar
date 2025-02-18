9a8c4bdf4cbd2d14c94d8a4e2acda2e8d4de5140
nighilramachandran <nighilramm@gmail.com>
2025-02-18 10:59:33 +0400

    fix suggestion from lint in next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["www.skeidar.no"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.skeidar.no",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
