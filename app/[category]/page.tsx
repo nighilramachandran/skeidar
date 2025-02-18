9c46b5b2280d778b3d1a2686ee71d61da52ca0a4
Nighil <nighilramm@gmail.com>
2025-02-18 20:41:42 +0400

    constant routes path configs

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -1,7 +1,18 @@
+import { notFound } from "next/navigation";
 import React from "react";
+import { productsData } from "./data";
 
-const CategoryPage = (props: any) => {
-  //   console.log("props", props);
+interface Props {
+  params: Promise<{ category: string }>;
+}
+const CategoryPage = async ({ params }: Props) => {
+  const { category } = await params;
+  const products = productsData[category];
+
+  if (!products) notFound();
+
+  console.log("products", products);
+
   return <div>page</div>;
 };
 
---
+++ app/utils/RouteConfig.ts
@@ -0,0 +1,6 @@
+export const ROUTES = {
+  ROOT: "/",
+  ALL_PRODUCTS: "all-products",
+  ALL_BEDS: "all-beds",
+  CONTINENTAL_BEDS: "continental-beds",
+};
